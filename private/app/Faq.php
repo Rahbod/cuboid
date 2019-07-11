bfae9bd378928aca2cd1273e49f5225f20e5fbd2
rahimi <mr.rahimi@outlook.com>
2019-07-11 16:16:45 +0430

    add faq model assets

---
+++ private/app/Faq.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App;
+
+use Appnegar\Cms\Traits\ModelTrait;
+use Illuminate\Database\Eloquent\Model;
+
+class Faq extends Model
+{
+    use ModelTrait;
+    protected $fillable = ['author_id', 'question', 'answer', 'order', 'status'];
+
+    public static function mainFields()
+    {
+        return [
+            'name' => static::getTableName(),
+            'items' => [
+                [
+                    'name' => 'id',
+                    'type' => 'numeric',
+                    'input_type' => 'hidden',
+                    'orderable' => true,
+                    'searchable' => true,
+                    'show_in_table' => true,
+                    'show_in_sub_table' => false,
+                    'show_in_form' => true
+                ],
+                [
+                    'name' => 'author_id',
+                    'type' => 'numeric',
+                    'input_type' => 'hidden',
+                    'orderable' => true,
+                    'searchable' => true,
+                    'show_in_table' => false,
+                    'show_in_form' => true,
+                ],
+                [
+                    'name' => 'question',
+                    'type' => 'string',
+                    'input_type' => 'textarea',
+                    'orderable' => true,
+                    'searchable' => true,
+                    'show_in_table' => true,
+                    'show_in_form' => true
+                ],
+                [
+                    'name' => 'answer',
+                    'type' => 'string',
+                    'input_type' => 'editor',
+                    'orderable' => true,
+                    'searchable' => true,
+                    'show_in_table' => false,
+                    'show_in_form' => true
+                ],
+                [
+                    'name' => 'order',
+                    'type' => 'numeric',
+                    'input_type' => 'number',
+                    'orderable' => true,
+                    'searchable' => true,
+                    'show_in_table' => true,
+                    'show_in_sub_table' => false,
+                    'show_in_form' => true
+                ],
+                [
+                    'name' => 'status',
+                    'type' => 'select',
+                    'input_type' => 'radio',
+                    'orderable' => true,
+                    'searchable' => true,
+                    'show_in_table' => true,
+                    'show_in_sub_table' => false,
+                    'show_in_form' => true,
+                    'options' => [['id' => 0, 'text' => 'inactive'], ['id' => 1, 'text' => 'active']]
+                ],
+                [
+                    'name' => 'created_at',
+                    'type' => 'date',
+                    'input_type' => 'date',
+                    'orderable' => true,
+                    'searchable' => true,
+                    'show_in_table' => false,
+                    'show_in_form' => false,
+                ],
+                [
+                    'name' => 'updated_at',
+                    'type' => 'date',
+                    'input_type' => 'date',
+                    'orderable' => true,
+                    'searchable' => true,
+                    'show_in_table' => false,
+                    'show_in_form' => false,
+                ]
+            ]
+        ];
+    }
+
+    public static function relatedFields()
+    {
+        return [
+            [
+                'name' => 'author',
+                'table' => User::getTableName(),
+                'show_in_form' => false,
+                'show_in_table' => false,
+                'items' => User::getSubFields()
+            ]
+        ];
+    }
+
+    public function author()
+    {
+        return $this->belongsTo('App\User', 'author_id', 'id');
+    }
+}
