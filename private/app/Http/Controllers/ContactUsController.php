9b78cae9bdfb7259869e48f23da22ecccf2be46d
rezae <mustafa.rezae01@gmail.com>
2019-07-15 19:55:18 +0430

    responsiving the home page

--- private/app/Http/Controllers/ContactUsController.php
+++ private/app/Http/Controllers/ContactUsController.php
@@ -8,13 +8,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Faq;
+use App\Project;
 use Illuminate\Http\Request;
 
 class ContactUsController extends Controller
 {
     public function show()
     {
-        return view('main_site.pages.contact_us')->with('sub_page','subPage');
+        $categories = Category::where('status', 1)->where('type', 'project')->with(['projects' => function ($query) {
+            $query->where('status', 1);
+            $query->orderBy('order', 'asc');
+            $query->take(8);
+        }])->orderBy('order', 'asc')->take(4)->get();
+        $faqs = Faq::where('status', 1)->orderBy('order', 'asc')->take(10)->get();
+
+        return view('main_site.pages.contact_us')->with(['categories' => $categories, 'faqs' => $faqs, 'sub_page' => 'subPage']);;
     }
 
     public function store(Request $request)
