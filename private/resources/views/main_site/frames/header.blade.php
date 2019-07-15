e91ed8c2baf7ae2768951989f44aa91324760716
rezae <mustafa.rezae01@gmail.com>
2019-07-15 17:36:59 +0430

    responsiving the home page

--- private/resources/views/main_site/frames/header.blade.php
+++ private/resources/views/main_site/frames/header.blade.php
@@ -8,28 +8,29 @@
                 </div>
             </div>
         </div>
+        <div class="smallHeader">
+            <div class="container px-md-4">
+                <div class="row">
+                    <div class="col-6 text-right">
+                        <div class="smallHeader__title">
+                            <p class="upTitle">سقف الوفل</p>
+                            <h1 class="title">غيوبويد</h1>
+                            <span class="subTitle">خيار طبيعی</span>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="d-flex align-items-center h-100 justify-content-end">
+                            <a href="javascript:;" class="text-center smallHeader__logo">
+                                <img src="{{asset('/assets/site/media/images/downloads/CUBOID-white.png')}}" alt="">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div class="mainSlider owl-carousel" id="mainSlider">
             <div class="mainSlider--item">
-                <div class="smallHeader">
-                    <div class="container px-md-4">
-                        <div class="row">
-                            <div class="col-6 text-right">
-                                <div class="smallHeader__title">
-                                    <p class="upTitle">سقف الوفل</p>
-                                    <h1 class="title">غيوبويد</h1>
-                                    <span class="subTitle">خيار طبيعی</span>
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="d-flex align-items-center h-100 justify-content-end">
-                                    <a href="javascript:;" class="text-center smallHeader__logo">
-                                        <img src="{{asset('/assets/site/media/images/downloads/CUBOID-white.png')}}" alt="">
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <img src="{{asset('/assets/site/media/images/landing/landing-2.jpg')}}" alt="">
                 <div class="leftBox">
@@ -45,26 +46,6 @@
                 </div>
             </div>
             <div class="mainSlider--item">
-                <div class="smallHeader">
-                    <div class="container px-md-4">
-                        <div class="row">
-                            <div class="col-6 text-right">
-                                <div class="smallHeader__title">
-                                    <p class="upTitle">سقف الوفل</p>
-                                    <h1 class="title">غيوبويد</h1>
-                                    <span class="subTitle">خيار طبيعی</span>
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="d-flex align-items-center h-100 justify-content-end">
-                                    <a href="javascript:;" class="text-center smallHeader__logo">
-                                        <img src="{{asset('/assets/site/media/images/downloads/CUBOID-white.png')}}" alt="">
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <img src="{{asset('/assets/site/media/images/landing/landing-2.jpg')}}" alt="">
                 <div class="leftBox">
@@ -80,26 +61,6 @@
                 </div>
             </div>
             <div class="mainSlider--item">
-                <div class="smallHeader">
-                    <div class="container px-md-4">
-                        <div class="row">
-                            <div class="col-6 text-right">
-                                <div class="smallHeader__title">
-                                    <p class="upTitle">سقف الوفل</p>
-                                    <h1 class="title">غيوبويد</h1>
-                                    <span class="subTitle">خيار طبيعی</span>
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="d-flex align-items-center h-100 justify-content-end">
-                                    <a href="javascript:;" class="text-center smallHeader__logo">
-                                        <img src="{{asset('/assets/site/media/images/downloads/CUBOID-white.png')}}" alt="">
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
                 <img src="{{asset('/assets/site/media/images/landing/landing-2.jpg')}}" alt="">
                 <div class="leftBox">
