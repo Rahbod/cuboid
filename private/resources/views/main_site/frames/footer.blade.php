abea1aa99c60d96c39fc4195096ddcd2459f6654
rezae <mustafa.rezae01@gmail.com>
2019-07-17 19:21:54 +0430

    home page

--- private/resources/views/main_site/frames/footer.blade.php
+++ private/resources/views/main_site/frames/footer.blade.php
@@ -21,95 +21,97 @@
                             </div>
                         </div>
                         <div class="form-row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="">القسم المطلوب</label>
-                                    <select tabindex="1" name="relevant_section" class="custom-select" id="">
-                                        @if(config('system.relevant_section'))
-                                            <option selected>انتخب ...</option>
-                                            @foreach(config('system.relevant_section') as$key=> $option)
-                                                <option value="{{$key}}">{{$option}}</option>
-                                            @endforeach
-                                        @else
-                                            <option selected>انتخب ...</option>
-                                            <option value="1">مدیر</option>
-                                            <option value="2">المنتجات</option>
-                                            <option value="3">متنوع</option>
-                                        @endif
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="">الاسم الأول واللقب</label>
-                                    <input tabindex="2" name="name" type="text" class="form-control" id="2"
-                                           placeholder="الاسم الأول واللقب">
-                                </div>
-                                <div class="form-group">
-                                    <label for="">البريد الإلكتروني</label>
-                                    <input tabindex="3" name="email" type="email" class="form-control" id="3"
-                                           placeholder="example@email.com">
-                                </div>
+                            <div class="col-md-6 form-group">
+                                <label for="">القسم المطلوب</label>
+                                <select tabindex="1" name="relevant_section" class="custom-select" id="">
+                                    @if(config('system.relevant_section'))
+                                        <option selected>انتخب ...</option>
+                                        @foreach(config('system.relevant_section') as$key=> $option)
+                                            <option value="{{$key}}">{{$option}}</option>
+                                        @endforeach
+                                    @else
+                                        <option selected>انتخب ...</option>
+                                        <option value="1">مدیر</option>
+                                        <option value="2">المنتجات</option>
+                                        <option value="3">متنوع</option>
+                                    @endif
+                                </select>
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="text">وصف</label>
-                                    <textarea tabindex="5" name="content" class="form-control" id="content"
-                                              placeholder="وصف"
-                                              required cols="30" rows="8"></textarea>
-                                    <div class="invalid-tooltip"></div>
-                                </div>
+                            <div class="col-md-6 form-group">
+                                <label for="">الاسم الأول واللقب</label>
+                                <input tabindex="2" name="name" type="text" class="form-control" id="2"
+                                       placeholder="الاسم الأول واللقب">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label for="">البريد الإلكتروني</label>
+                                <input tabindex="3" name="email" type="email" class="form-control" id="3"
+                                       placeholder="example@email.com">
+                            </div>
+                            <div class="col-md-6 form-group">
+                                <label for="">رقم الهاتف المحمول</label>
+                                <input tabindex="4" name="phone" type="text" class="form-control" id="4"
+                                       placeholder="09xxxxxxxxx">
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <label for="text">وصف</label>
+                                <textarea tabindex="5" name="content" class="form-control" id="content"
+                                          placeholder="وصف"
+                                          required cols="30" rows="8"></textarea>
+                                <div class="invalid-tooltip"></div>
                             </div>
 
-                            {{--<div class="form-group col-md-6">--}}
-                            {{--<label for="">رقم الهاتف المحمول</label>--}}
-                            {{--<input tabindex="4" name="phone" type="text" class="form-control" id="4"--}}
-                            {{--placeholder="09xxxxxxxxx">--}}
-                            {{--</div>--}}
-
-                            {{--<div class="form-group col-md-6" tabindex="5">--}}
-                            {{--<div class="d-flex justify-content-between" id="captcha_image">--}}
-                            {{--<div class="captchaImageContainer">--}}
-                            {{--{!! captcha_img('flat'); !!}--}}
-                            {{--</div>--}}
-                            {{--<a style="padding: 10px 16px;" data-lang="{{session('lang')}}"--}}
-                            {{--href="javascript:;"--}}
-                            {{--class="renewCaptchaImage btn border ml-2">--}}
-                            {{--<i class="far fa-redo"></i>--}}
-                            {{--</a>--}}
-                            {{--</div>--}}
-                            {{--<input type="text" class="form-control"--}}
-                            {{--autocomplete="captcha"--}}
-                            {{--spellcheck="false"--}}
-                            {{--tabindex="3"--}}
-                            {{--name="captcha" required--}}
-                            {{--id="captcha">--}}
-                            {{--@if ($errors->has('captcha'))--}}
-                            {{--<div class="invalid-feedback" style="display: block;">--}}
-                            {{--{{ $errors->first('captcha') }}--}}
-                            {{--</div>--}}
-                            {{--@endif--}}
-                            {{--</div>--}}
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-6">
-                                <div class="d-flex" id="captcha_image">
+                            <div class="form-group col-md-6" tabindex="5">
+                                <div class="d-flex justify-content-between" id="captcha_image">
                                     <div class="captchaImageContainer">
-                                        <img src="{{asset('assets/site/media/images/footer/captcha.png')}}">
+                                        {!! captcha_img('flat'); !!}
                                     </div>
-                                    <a href="void:;" class="renewCaptchaImage btn">
+                                    <a style="padding: 10px;" data-lang="{{session('lang')}}"
+                                       href="javascript:;"
+                                       class="renewCaptchaImage btn border ml-2">
                                         <i class="far fa-redo"></i>
                                     </a>
-                                    <input style="color: #f68e24;" type="text"
-                                           class="form-control securityCode flex-fill" id="5"
-                                           placeholder="تصویر امنیتی">
+                                    <input type="text" class="form-control"
+                                           autocomplete="captcha"
+                                           spellcheck="false"
+                                           tabindex="3"
+                                           name="captcha" required
+                                           id="captcha">
                                 </div>
-                            </div>
 
+                                @if ($errors->has('captcha'))
+                                    <div class="invalid-feedback" style="display: block;">
+                                        {{ $errors->first('captcha') }}
+                                    </div>
+                                @endif
+                            </div>
                             <div class="form-group col-md-6">
                                 <button tabindex="6" type="submit" class="btn submitBtn form-control">إرسال إلى القسم ذي
                                     الصلة
                                 </button>
                             </div>
+
                         </div>
+                        {{--<div class="form-row">--}}
+                            {{--<div class="form-group col-md-6">--}}
+                                {{--<div class="d-flex" id="captcha_image">--}}
+                                    {{--<div class="captchaImageContainer">--}}
+                                        {{--<img src="{{asset('assets/site/media/images/footer/captcha.png')}}">--}}
+                                    {{--</div>--}}
+                                    {{--<a href="void:;" class="renewCaptchaImage btn">--}}
+                                        {{--<i class="far fa-redo"></i>--}}
+                                    {{--</a>--}}
+                                    {{--<input style="color: #f68e24;" type="text"--}}
+                                           {{--class="form-control securityCode flex-fill" id="5"--}}
+                                           {{--placeholder="تصویر امنیتی">--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+
+                            {{--<div class="form-group col-md-6">--}}
+                                {{--<button tabindex="6" type="submit" class="btn submitBtn form-control">إرسال إلى القسم ذي--}}
+                                    {{--الصلة--}}
+                                {{--</button>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
                     </form>
                 </div>
             </div>
